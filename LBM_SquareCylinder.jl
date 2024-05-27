9db59c516c61779c9221c305ccd601889b76ea62
Gabriel Suchodolak de Souza <157864271+Arkames@users.noreply.github.com>
2024-05-27 15:14:46 -0300

    Add files via upload

---
+++ LBM_SquareCylinder.jl
@@ -0,0 +1,5 @@
+# LBM_SquareCylinder.jl
+# Julia implementation of Lattice-Boltzmann Method for simulating 2D flow around a square cylinder.
+# Evaluates velocity profiles, recirculation length, and drag coefficient for various Reynolds numbers.
+
+
