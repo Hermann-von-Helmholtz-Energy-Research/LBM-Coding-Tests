363599f19992b3f727da832733cd2859d46c8451
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2024-06-26 11:46:34 -0300

    UPD switched from C90/ANSI to C99 standard

--- ENCIT2024/taylor-green/main.c
+++ ENCIT2024/taylor-green/main.c
@@ -1,12 +1,20 @@
-
-/* Constants */
-const unsigned int scale = 1;
-const unsigned int NX = 32 * scale;
-const unsigned int NY = NX;
+/* Includes */
+#include <stdlib.h>
 
 /* Lattice */
+const unsigned int scale = 1; /* multiples of 2 up to 64 */
+const unsigned int NX = 32 * scale;
+const unsigned int NY = NX;
 const unsigned int ndir = 9; /* D2Q9 */
-const unsigned long int mem_size_ndir = sizeof(double) * NX * NY * ndir;
+const size_t mem_size_ndir = sizeof(double) * NX * NY * ndir;
+
+/* Time Steps */
+const unsigned int NSTEPS = 204800 / scale / scale;
+
+// Index Functions
+inline size_t scalar_index(unsigned int x, unsigned int y) {
+    return (size_t)(NX) * (size_t)(y) + (size_t)(x);
+}
 
 /* Main */
 int main (int argc, char* argv[]){
