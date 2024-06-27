d1cdb35a285621a94aeab46e3c14321198e2d274
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2024-06-27 00:07:52 -0300

    UPD the main.jl

--- ENCIT2024/taylor-green-julia/main.jl
+++ ENCIT2024/taylor-green-julia/main.jl
@@ -1,13 +1,13 @@
 # Precision parameters
-const 𝕋     = Float64
+const 𝕋                 = Float64                       # Absent in the ref. C99 code
 
 # Lattice constants
 const scale             = UInt(1)
-const chunk             = UInt(32)
+const chunk             = UInt(32)                      # Hardcoded in the ref. C99 code
 const NX                = UInt(scale * chunk)
 const NY                = NX
 const ndir              = UInt(9)
 const mem_size_scalar   = UInt(NX * NY * sizeof(𝕋))
-const mem_size_vector   = UInt(mem_size_scalar * ndir)
+const mem_size_vector   = UInt(mem_size_scalar * ndir)  # Optimized w/respect to ref. C99 code
 
 
