190659e736512092f2ff97f52c5cc53ced551dee
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2024-06-29 18:34:56 -0300

    UPD the 11-ORIG-julia-BaseVector.jl

--- ENCIT2024/taylor-green-julia/11-ORIG-julia-BaseVector.jl
+++ ENCIT2024/taylor-green-julia/11-ORIG-julia-BaseVector.jl
@@ -9,6 +9,20 @@
 `init(ℙ::Type{<:AbstractFloat}, l::Int)::Dict{Symbol, Dict}`\n
 Computes (i) types, (ii) case, (iii) lattice, and (iv) properties simulation parameters, and
 returns as a `Dict{Symbol, Dict}`.
+
+```julia-REPL
+julia> @benchmark init(Float64, 0)
+BenchmarkTools.Trial: 10000 samples with 10 evaluations.
+ Range (min … max):  1.478 μs … 323.095 μs  ┊ GC (min … max):  0.00% … 97.87%
+ Time  (median):     1.558 μs (↓)           ┊ GC (median):     0.00%
+ Time  (mean ± σ):   1.798 μs ±   6.372 μs  ┊ GC (mean ± σ):  10.97% ±  3.24%
+              ↓
+        ▄▆▇██▇┊▅▄▄▂▁▁
+  ▂▃▃▅▆███████┊██████▇▇▅▄▄▃▂▂▂▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ ▃
+  1.48 μs         Histogram: frequency by time        1.87 μs <
+
+ Memory estimate: 4.91 KiB, allocs estimate: 50.
+```
 """
 function init(ℙ::Type{𝕋} where 𝕋<:AbstractFloat,    # The floating point precision
               l::Int)                               # log2(scale)
